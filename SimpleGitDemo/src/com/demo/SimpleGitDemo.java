075c3f70c8726aa74423a1c7fea9c53a27ef44b1
Amit Chaudhury <amitchy07@gmail.com>
2020-09-19 15:34:01 +0530

    first Commit

---
+++ SimpleGitDemo/src/com/demo/SimpleGitDemo.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class SimpleGitDemo {
+
+	public static void main(String[] args) {
+		boolean flag=true;
+		
+		if(flag)
+		{
+			System.out.println("Simple Git Demo Project");
+		}
+		else
+		{
+			System.out.println("Simple Git demo else portion");
+		}
+		
+
+	}
+
+}
